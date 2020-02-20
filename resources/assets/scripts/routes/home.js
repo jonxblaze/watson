bb10f24d6e01796f30d081aeda471bbe8b216f86
Jon B <jonxblaze@gmail.com>
2020-02-20 17:01:00 -0600

    Fixed node-sass error

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -1,8 +1,8 @@
 export default {
-  init() {
-    // JavaScript to be fired on the home page
-  },
-  finalize() {
-    // JavaScript to be fired on the home page, after the init JS
-  },
+    init() {
+        console.log('HELLO');
+    },
+    finalize() {
+        console.log('FINALIZED');
+    },
 };
